be94fdeb11881c4a8629c0352118947d3c4a4337
Anthony-nemer <anthonynemer000@gmail.com>
2025-01-27 19:19:46 +0200

    ..

--- src/components/OnGoingEvents/OngoingEvents.js
+++ src/components/OnGoingEvents/OngoingEvents.js
@@ -19,10 +19,29 @@ function OngoingEvents({ userInfo }) {
         { field: 'attendance_number', headerName: 'Nb. of People', flex: 1, align: 'center', headerAlign: 'center' },
         { field: 'number_of_tables', headerName: 'Nb. of Tables', flex: 1, align: 'center', headerAlign: 'center' },
         { field: 'persons_per_table', headerName: 'Persons per Table', flex: 1, align: 'center', headerAlign: 'center' },
+        {
+            field: 'action',
+            headerName: 'Payment',
+            width: 150,
+            renderCell: (params) => {
+                return (
+                    params.row.status === 'Payment Pending' ? (
+                        <button
+                            className="pay-button"
+                            onClick={() => handlePaymentClick(params.row.event_id)}
+                        >
+                            Pay Now
+                        </button>
+                    ) : null
+                );
+            },
+            align: 'center',
+            headerAlign: 'center',
+        },
     ];
 
     const formatDate = (date) => {
-        return format(new Date(date), 'dd-MM-yyyy'); 
+        return format(new Date(date), 'dd-MM-yyyy');
     };
 
     const getActiveEvents = async () => {
@@ -30,16 +49,20 @@ function OngoingEvents({ userInfo }) {
 
         try {
             const response = await axios.get(`${baseUrl}/get-events`, {
-                params: { userId }, 
+                params: { userId },
             });
             setOngoingEvents(response.data.map(event => ({
                 ...event,
                 id: event.event_id,
-                event_date: formatDate(event.event_date), 
+                event_date: formatDate(event.event_date),
             })));
         } catch (error) {
             console.error("Error fetching user's events: ", error);
         }
+    };
+
+    const handlePaymentClick = (eventId) => {
+        console.log(`Initiating payment for event ID: ${eventId}`);
     };
 
     useEffect(() => {
@@ -49,7 +72,7 @@ function OngoingEvents({ userInfo }) {
     }, [userInfo]);
 
     return (
-        <div className='body'>
+        <div className="body">
             <div className="ongoing">
                 <DataGrid
                     rows={ongoingEvents}
